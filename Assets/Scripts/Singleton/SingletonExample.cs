126de0a6feced2c4cbf3332831b4c628f06c4d70
cosmos53076 <cosmos53076@163.com>
2016-03-27 22:40:35 +0800

    Initialize project.

---
+++ Assets/Scripts/Singleton/SingletonExample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace QuickUnity.Examples.Singleton
+{
+    /// <summary>
+    /// Class SingletonExample.
+    /// </summary>
+    public class SingletonExample : MonoBehaviour
+    {
+        // Use this for initialization
+        private void Start()
+        {
+            TestSingletonMonoBehaviourOne testBehaivourOne = TestSingletonMonoBehaviourOne.instance;
+            TestSingletonMonoBehaviourTwo testBehaivourTwo = TestSingletonMonoBehaviourTwo.instance;
+            testBehaivourOne.Run();
+            testBehaivourTwo.Run();
+        }
+    }
+}
