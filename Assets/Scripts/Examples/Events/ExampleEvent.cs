2aa3f0cb7907fec066fe91a70486c3f9d3de7dd8
Jerry Lee <cosmos53076@163.com>
2016-04-04 18:59:10 +0800

    Add Example scripts for EventDispatcher.

---
+++ Assets/Scripts/Examples/Events/ExampleEvent.cs
@@ -0,0 +1,27 @@
+﻿using QuickUnity.Events;
+
+namespace QuickUnity.Examples.Events
+{
+    /// <summary>
+    /// The example event object.
+    /// </summary>
+    /// <seealso cref="QuickUnity.Events.Event" />
+    public class ExampleEvent : Event
+    {
+        /// <summary>
+        /// The example event type.
+        /// </summary>
+        public const string Example = "Example";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExampleEvent"/> class.
+        /// </summary>
+        /// <param name="eventType">The type of event.</param>
+        /// <param name="context">The context object.</param>
+        public ExampleEvent(string eventType, object context = null)
+            :base(eventType, context)
+        {
+
+        }
+    }
+}
