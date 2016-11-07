d9c25af4eb0e212317a305048e5219bfcda6a358
cosmos53076 <cosmos53076@163.com>
2016-11-07 17:48:46 +0800

    Add serial port communication classes.

---
+++ Assets/QuickUnity/Scripts/IO/Ports/Serial.cs
@@ -0,0 +1,196 @@
+﻿/*
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2016 Jerry Lee
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+using QuickUnity.Core.Miscs;
+using QuickUnity.Events;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace QuickUnity.IO.Ports
+{
+    /// <summary>
+    /// Serial is used for communication between serial ports.
+    /// </summary>
+    /// <seealso cref="QuickUnity.Events.ThreadEventDispatcher"/>
+    public class Serial : ThreadEventDispatcher
+    {
+        /// <summary>
+        /// The serial port object.
+        /// </summary>
+        private SerialPort m_serialPort;
+
+        /// <summary>
+        /// The thread for receiving data.
+        /// </summary>
+        private Thread m_receiveDataThread;
+
+        /// <summary>
+        /// The serial data packet handler.
+        /// </summary>
+        private ISerialPacketHandler m_packetHandler;
+
+        /// <summary>
+        /// Gets or sets the serial data packet handler.
+        /// </summary>
+        /// <value>The serial data packet handler.</value>
+        public ISerialPacketHandler packetHandler
+        {
+            get { return m_packetHandler; }
+            set { m_packetHandler = value; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the serial port is open.
+        /// </summary>
+        /// <value><c>true</c> if the serial port is open; otherwise, <c>false</c>.</value>
+        public bool isOpen
+        {
+            get { return m_serialPort != null && m_serialPort.IsOpen; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Serial"/> class.
+        /// </summary>
+        /// <param name="portName">The port to use (for example, COM1).</param>
+        /// <param name="baudRate">The baud rate.</param>
+        /// <param name="parity">One of the Parity values.</param>
+        /// <param name="dataBits">The data bits value.</param>
+        /// <param name="stopBits">One of the StopBits values.</param>
+        public Serial(string portName, int baudRate, Parity parity = Parity.None,
+            int dataBits = 8, StopBits stopBits = StopBits.One)
+        {
+            m_serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+        }
+
+        #region Public Functions
+
+        /// <summary>
+        /// Opens a new serial port connection.
+        /// </summary>
+        public void Open()
+        {
+            try
+            {
+                if (m_serialPort != null && !m_serialPort.IsOpen)
+                {
+                    m_serialPort.WriteTimeout = 500;
+                    m_serialPort.ReadTimeout = 500;
+                    m_serialPort.Open();
+                    DispatchEvent(new SerialEvent(SerialEvent.Open, this));
+
+                    // if has old thread, then abort it.
+                    if (m_receiveDataThread != null)
+                    {
+                        m_receiveDataThread.Abort();
+                    }
+
+                    m_receiveDataThread = new Thread(ReceiveData);
+                    m_receiveDataThread.Start();
+                }
+            }
+            catch (Exception exception)
+            {
+                DebugLogger.LogException(exception, this);
+            }
+        }
+
+        /// <summary>
+        /// Sends the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void Send(object data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            if (m_packetHandler != null)
+            {
+                ISerialPacket packet = m_packetHandler.Pack(data);
+
+                if (packet != null && isOpen)
+                {
+                    m_serialPort.Write(packet.bytes, 0, packet.bytes.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Closes the port connection.
+        /// </summary>
+        public void Close()
+        {
+            try
+            {
+                if (m_receiveDataThread != null && m_receiveDataThread.IsAlive)
+                {
+                    m_receiveDataThread.Abort();
+                }
+
+                if (isOpen)
+                {
+                    m_serialPort.Close();
+                    DispatchEvent(new SerialEvent(SerialEvent.Close, this));
+                }
+            }
+            catch (Exception exception)
+            {
+                DebugLogger.LogException(exception);
+            }
+        }
+
+        #endregion Public Functions
+
+        /// <summary>
+        /// Receives the serial port data.
+        /// </summary>
+        private void ReceiveData()
+        {
+            while (isOpen)
+            {
+                try
+                {
+                    byte receivedByte = (byte)m_serialPort.ReadChar();
+
+                    // Disptach data event.
+                    if (m_packetHandler != null)
+                    {
+                        ISerialPacket[] dataPackets = m_packetHandler.Unpack(new byte[1] { receivedByte });
+
+                        if (dataPackets != null && dataPackets.Length > 0)
+                        {
+                            DispatchEvent(new SerialEvent(SerialEvent.Data, this, dataPackets));
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    DebugLogger.LogException(exception);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Tests/IntegrationTests/SerialTests/SerialTest.cs
@@ -0,0 +1,68 @@
+﻿using QuickUnity.IO.Ports;
+using UnityEngine;
+
+namespace QuickUnity.Tests.IntegrationTests
+{
+    /// <summary>
+    /// Integration test of Serial.
+    /// </summary>
+    /// <seealso cref="UnityEngine.MonoBehaviour"/>
+    [IntegrationTest.DynamicTestAttribute("SerialTests")]
+    [IntegrationTest.SucceedWithAssertions]
+    [IntegrationTest.Timeout(10)]
+    public class SerialTest : MonoBehaviour
+    {
+        private Serial m_serial;
+
+        /// <summary>
+        /// Start is called just before any of the Update methods is called the first time.
+        /// </summary>
+        private void Start()
+        {
+            m_serial = new Serial("COM3", 9600);
+            m_serial.packetHandler = new SerialTestsPacketHandler();
+            m_serial.AddEventListener(SerialEvent.Data, OnSerialData);
+            m_serial.Open();
+            m_serial.Send("a");
+            Invoke("Pass", 9f);
+        }
+
+        private void Update()
+        {
+            if (m_serial != null)
+            {
+                m_serial.Update();
+            }
+        }
+
+        /// <summary>
+        /// Called when receive [serial data].
+        /// </summary>
+        /// <param name="evt">The evt.</param>
+        private void OnSerialData(Events.Event evt)
+        {
+            SerialEvent serialEvent = evt as SerialEvent;
+            ISerialPacket[] dataPackets = serialEvent.dataPackets;
+
+            for (int i = 0, length = dataPackets.Length; i < length; ++i)
+            {
+                SerialTestPacket packet = dataPackets[i] as SerialTestPacket;
+                Debug.Log(packet.data);
+            }
+        }
+
+        /// <summary>
+        /// Passes the test.
+        /// </summary>
+        private void Pass()
+        {
+            if (m_serial != null)
+            {
+                m_serial.RemoveEventListener(SerialEvent.Data, OnSerialData);
+                m_serial.Close();
+            }
+
+            IntegrationTest.Pass(gameObject);
+        }
+    }
+}
