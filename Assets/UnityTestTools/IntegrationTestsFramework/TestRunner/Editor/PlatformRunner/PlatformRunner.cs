5cbc044b82f2e39d939e54bb62f084ae15d5efb6
Jerry Lee <cosmos53076@163.com>
2016-11-28 22:00:33 +0800

    Convert line format to Windows.

--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/PlatformRunner/PlatformRunner.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/PlatformRunner/PlatformRunner.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
-using System.Linq;
 
 namespace UnityTest.IntegrationTests
 {
@@ -74,7 +74,7 @@ namespace UnityTest.IntegrationTests
                 settings.AddConfigurationFile(TestRunnerConfigurator.integrationTestsNetwork,
                                               string.Join("\n", configuration.GetConnectionIPs()));
 
-            settings.AddConfigurationFile (TestRunnerConfigurator.testScenesToRun, string.Join ("\n", configuration.testScenes.ToArray()));
+            settings.AddConfigurationFile(TestRunnerConfigurator.testScenesToRun, string.Join("\n", configuration.testScenes.ToArray()));
 
             settings.ChangeSettingsForIntegrationTests();
 
@@ -109,21 +109,25 @@ namespace UnityTest.IntegrationTests
             {
                 case BuildTargetGroup.Android:
                     return BuildTarget.Android;
-                case BuildTargetGroup.WebPlayer:
-                    return BuildTarget.WebPlayer;
+
+                case BuildTargetGroup.WebGL:
+                    return BuildTarget.WebGL;
+
                 default:
-                {
-                    switch (Application.platform)
                     {
-                        case RuntimePlatform.WindowsPlayer:
-                            return BuildTarget.StandaloneWindows;
-                        case RuntimePlatform.OSXPlayer:
-                            return BuildTarget.StandaloneOSXIntel;
-                        case RuntimePlatform.LinuxPlayer:
-                            return BuildTarget.StandaloneLinux;
+                        switch (Application.platform)
+                        {
+                            case RuntimePlatform.WindowsPlayer:
+                                return BuildTarget.StandaloneWindows;
+
+                            case RuntimePlatform.OSXPlayer:
+                                return BuildTarget.StandaloneOSXIntel;
+
+                            case RuntimePlatform.LinuxPlayer:
+                                return BuildTarget.StandaloneLinux;
+                        }
+                        return BuildTarget.WebGL;
                     }
-                    return BuildTarget.WebPlayer;
-                }
             }
         }
     }
