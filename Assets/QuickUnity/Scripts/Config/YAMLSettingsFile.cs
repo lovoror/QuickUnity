6cab6cb71796cb6460315e2155a04becd66366b2
cosmos53076 <cosmos53076@163.com>
2016-12-06 18:20:55 +0800

    Add class YAMLSettingsFile to process the file of format YAML.

---
+++ Assets/QuickUnity/Scripts/Config/YAMLSettingsFile.cs
@@ -0,0 +1,108 @@
+﻿/*
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2016 Jerry Lee
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+using QuickUnity.Core.Miscs;
+using System;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace QuickUnity.Config
+{
+    /// <summary>
+    /// Contains methods for processing the YAML format settings file.
+    /// </summary>
+    public class YAMLSettingsFile
+    {
+        /// <summary>
+        /// The file extension of setting file.
+        /// </summary>
+        private const string SettingsFileExtension = ".asset";
+
+        /// <summary>
+        /// Serializes the specified object at file path.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <param name="source">The specified obejct.</param>
+        /// <param name="autoGenerateFileName">if set to <c>true</c> [automatic generate file name].</param>
+        public static void Serialize(string filePath, object source, bool autoGenerateFileName = true)
+        {
+            if (string.IsNullOrEmpty(filePath) || source == null)
+            {
+                return;
+            }
+
+            try
+            {
+                if (autoGenerateFileName)
+                {
+                    string fileName = source.GetType().Name + SettingsFileExtension;
+                    filePath = Path.Combine(filePath, fileName);
+                }
+
+                StreamWriter writer = File.CreateText(filePath);
+                Serializer serializer = new Serializer();
+                serializer.Serialize(writer, source);
+                writer.Close();
+            }
+            catch (Exception exception)
+            {
+                DebugLogger.LogException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes the specified file path.
+        /// </summary>
+        /// <typeparam name="T">The type definition of the object deserialized.</typeparam>
+        /// <param name="filePath">The file path.</param>
+        /// <param name="autoParseFileName">if set to <c>true</c> [automatic parse file name].</param>
+        /// <returns>T The object deserialized.</returns>
+        public static T Deserialize<T>(string filePath, bool autoParseFileName = true)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                try
+                {
+                    if (autoParseFileName)
+                    {
+                        string fileName = typeof(T).Name + SettingsFileExtension;
+                        filePath = Path.Combine(filePath, fileName);
+                    }
+
+                    StreamReader reader = File.OpenText(filePath);
+                    Deserializer deserializer = new Deserializer();
+                    T obj = deserializer.Deserialize<T>(reader);
+                    reader.Close();
+                    return obj;
+                }
+                catch (Exception exception)
+                {
+                    DebugLogger.LogException(exception);
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ Assets/Tests/UnitTests/Editor/YAMLSettingsFileTest.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using QuickUnity.Config;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace QuickUnity.Tests.UnitTests.ConfigTests
+{
+    /// <summary>
+    /// Unit test cases for class INIConfigFile.
+    /// </summary>
+    [TestFixture]
+    [Category("QuickUnity Tests/Unit Tests/YAMLSettingsFile Test")]
+    internal class YAMLSettingsFileTest
+    {
+        internal class TestSettings
+        {
+            public int intValue
+            {
+                get;
+                set;
+            }
+
+            public float floatValue
+            {
+                get;
+                set;
+            }
+
+            public string stringValue
+            {
+                get;
+                set;
+            }
+
+            public List<bool> boolListValue
+            {
+                get;
+                set;
+            }
+
+            public Dictionary<string, bool> boolDictValue
+            {
+                get;
+                set;
+            }
+        }
+
+        /// <summary>
+        /// Test case for method Serialize
+        /// </summary>
+        [Test]
+        public void SerializeTest()
+        {
+            TestSettings settings = new TestSettings();
+            settings.intValue = int.MaxValue;
+            settings.floatValue = float.MaxValue;
+            settings.stringValue = "test你好！";
+            settings.boolListValue = new List<bool>()
+            {
+                true, false, true, false
+            };
+            settings.boolDictValue = new Dictionary<string, bool>()
+            {
+                { "test1", true },
+                { "测试2", false }
+            };
+
+            YAMLSettingsFile.Serialize(Application.persistentDataPath, settings);
+            string filePath = Path.Combine(Application.persistentDataPath, "TestSettings.asset");
+            Assert.IsTrue(File.Exists(filePath));
+        }
+
+        /// <summary>
+        /// Test case for method Deserialize
+        /// </summary>
+        [Test]
+        public void DeserializeTest()
+        {
+            TestSettings settings = YAMLSettingsFile.Deserialize<TestSettings>(Application.persistentDataPath);
+
+            if (settings != null)
+            {
+                Assert.AreEqual(settings.intValue, int.MaxValue);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
