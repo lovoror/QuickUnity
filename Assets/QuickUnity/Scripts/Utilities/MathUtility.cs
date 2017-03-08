2d98c72e24f94ab3f6cf7d787b6fbf07d4a07756
cosmos53076 <cosmos53076@163.com>
2017-03-08 18:22:47 +0800

    Add method ClampAngle to MathUtility.

--- Assets/QuickUnity/Scripts/Utilities/MathUtility.cs
+++ Assets/QuickUnity/Scripts/Utilities/MathUtility.cs
@@ -100,5 +100,70 @@ namespace QuickUnity.Utilities
         {
             return IsOdd(Mathf.FloorToInt(n));
         }
+
+        /// <summary>
+        /// Clamps the angle between -180 and 180.
+        /// </summary>
+        /// <param name="angle">The angle.</param>
+        /// <param name="min">The minimum angle.</param>
+        /// <param name="max">The maximum angle.</param>
+        /// <returns>The angle between -180 and 180.</returns>
+        public static float ClampAngle(float angle, float min, float max)
+        {
+            angle = NormalizeAngle(angle);
+
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+            else if (angle < -180)
+            {
+                angle += 360;
+            }
+
+            min = NormalizeAngle(min);
+
+            if (min > 180)
+            {
+                min -= 360;
+            }
+            else if (min < -180)
+            {
+                min += 360;
+            }
+
+            max = NormalizeAngle(max);
+
+            if (max > 180)
+            {
+                max -= 360;
+            }
+            else if (max < -180)
+            {
+                max += 360;
+            }
+
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        /// <summary>
+        /// Normalizes the angle.
+        /// </summary>
+        /// <param name="angle">The angle.</param>
+        /// <returns>The normalized angle value.</returns>
+        public static float NormalizeAngle(float angle)
+        {
+            while (angle > 360)
+            {
+                angle -= 360;
+            }
+
+            while (angle < 0)
+            {
+                angle += 360;
+            }
+
+            return angle;
+        }
     }
 }
