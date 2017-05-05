ec2a5bfe72600d26f2ffdbbd93de4638d40e764a
Jerry Lee <cosmos53076@163.com>
2017-05-05 10:00:47 +0800

    Add BinaryTree.

--- Assets/QuickUnity/Scripts/Core/Collections/Generic/BinaryTree.cs
+++ Assets/QuickUnity/Scripts/Core/Collections/Generic/BinaryTree.cs
@@ -26,119 +26,275 @@ using System;
 
 namespace QuickUnity.Core.Collections.Generic
 {
+    /// <summary>
+    /// Represents a node in the BinaryTree&lt;T&gt;. This class cannot be inherited.
+    /// </summary>
+    /// <typeparam name="T">Specifies the element type of the binary tree.</typeparam>
+    public sealed class BinaryTreeNode<T> where T : IComparable
+    {
+        /// <summary>
+        /// The element contained in the node.
+        /// </summary>
+        private T m_element;
 
-	/// <summary>
-	/// The node of Binary Tree.
-	/// <typeparam name="T">Specifies the element type of the binary tree.</typeparam>
-	/// </summary>
-	public sealed class BinaryTreeNode<T> where T : IComparable<T>
-	{
-		/// <summary>
-		/// The <see cref="BinaryTree"/> that the BinaryTreeNode<T> belongs to.
-		/// </summary>
-		private BinaryTree<T> m_tree;
-		
-		/// <summary>
-		/// Gets the <see cref="BinaryTree"/> that the BinaryTreeNode<T> belongs to.
-		/// </summary>
-		/// <returns>The <see cref="BinaryTree"/> that the BinaryTreeNode<T> belongs to.</returns>
-		public BinaryTree<T> tree
-		{
-			get
-			{
-				return m_tree;
-			}
-		}
+        /// <summary>
+        /// Gets the element contained in the node.
+        /// </summary>
+        /// <value>The element contained in the node.</value>
+        public T element
+        {
+            get
+            {
+                return m_element;
+            }
 
-		private BinaryTreeNode<T> m_leftChild;
+            set
+            {
+                m_element = value;
+            }
+        }
 
-		public BinaryTreeNode<T> leftChild
-		{
-			get
-			{
-				return m_leftChild;
-			}
-		}
+        /// <summary>
+        /// The left child node of this node.
+        /// </summary>
+        private BinaryTreeNode<T> m_leftChild;
 
-		private BinaryTreeNode<T> m_rightChild;
+        /// <summary>
+        /// Gets the left child node of this node.
+        /// </summary>
+        /// <value>The left child node of this node.</value>
+        public BinaryTreeNode<T> leftChild
+        {
+            get
+            {
+                return m_leftChild;
+            }
 
-		public BinaryTreeNode<T> rightChild
-		{
-			get
-			{
-				return m_rightChild;
-			}
-		}
+            set
+            {
+                m_leftChild = value;
+            }
+        }
 
-		/// <summary>
-		/// The value contained in the node.
-		/// </summary>
-		private T m_value;
+        /// <summary>
+        /// The right child node of this node.
+        /// </summary>
+        private BinaryTreeNode<T> m_rightChild;
 
-		/// <summary>
-		/// Gets the value contained in the node.
-		/// </summary>
-		/// <returns>The value contained in the node.</returns>
-		public T value
-		{
-			get
-			{
-				return m_value;
-			}
+        /// <summary>
+        /// Gets the right child node of this node.
+        /// </summary>
+        /// <value>The right child node of this node.</value>
+        public BinaryTreeNode<T> rightChild
+        {
+            get
+            {
+                return m_rightChild;
+            }
 
-			set
-			{
-				m_value = value;
-			}
-		}
+            set
+            {
+                m_rightChild = value;
+            }
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="BinaryTreeNode"/> class.
-		/// </summary>
-		/// <param name="value">The value contained in the node.</param>
-		public BinaryTreeNode(T value)
-		{
-			m_value = value;
-			m_leftChild = null;
-			m_rightChild = null;
-		}
-	}
+        #region Constructors
 
-	/// <summary>
-	/// Binary Tree Data Structure.
-	/// </summary>
-    public class BinaryTree<T> where T : IComparable<T>
-	{
-		/// <summary>
-		/// The root node of the binray tree.
-		/// </summary>
-		private BinaryTreeNode<T> m_root;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryTreeNode{T}"/> class.
+        /// </summary>
+        /// <param name="element">The element to contain in the BinaryTree&lt;T&gt;.</param>
+        public BinaryTreeNode(T element)
+            : this(element, null, null)
+        {
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="BinaryTree"/> class.
-		/// </summary>
-		public BinaryTree()
-		{
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryTreeNode{T}"/> class.
+        /// </summary>
+        /// <param name="element">The element to contain in the BinaryTree&lt;T&gt;.</param>
+        /// <param name="leftChild">The left child node of this node.</param>
+        /// <param name="rightChild">The right child node of this node.</param>
+        public BinaryTreeNode(T element, BinaryTreeNode<T> leftChild, BinaryTreeNode<T> rightChild)
+        {
+            m_element = element;
+            m_leftChild = leftChild;
+            m_rightChild = rightChild;
+        }
 
-		}
+        #endregion Constructors
+    }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="BinaryTree"/> class.
-		/// </summary>
-		/// <param name="rootValue">The value contained in the root node.</param>
-		public BinaryTree(T rootValue)
-		{
-			m_root = new BinaryTreeNode<T>(rootValue);
-		}
+    /// <summary>
+    /// Represents binary tree data structure.
+    /// </summary>
+    /// <typeparam name="T">Specifies the element type of the binary tree.</typeparam>
+    public class BinaryTree<T> where T : IComparable
+    {
+        /// <summary>
+        /// The root node of this binary tree.
+        /// </summary>
+        protected BinaryTreeNode<T> m_root;
 
-		public void Insert(T value)
-		{
-			Insert(value, m_root);
-		}
+        /// <summary>
+        /// Gets the root node of this binary tree.
+        /// </summary>
+        /// <value>The root node of this binary tree.</value>
+        public BinaryTreeNode<T> root
+        {
+            get
+            {
+                return m_root;
+            }
 
-		public void Insert(T value, BinaryTreeNode<T> node)
-		{
-			
-		}
+            set
+            {
+                m_root = value;
+            }
+        }
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryTree{T}"/> class.
+        /// </summary>
+        public BinaryTree()
+        {
+            m_root = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryTree{T}"/> class.
+        /// </summary>
+        /// <param name="rootElement">The root element to contain in the BinaryTree&lt;T&gt;.</param>
+        public BinaryTree(T rootElement)
+        {
+            m_root = new BinaryTreeNode<T>(rootElement);
+        }
+
+        #endregion Constructors
+
+        #region Public Functions
+
+        #region Traversing Functions
+
+        /// <summary>
+        /// Preorder traverse the binary tree.
+        /// </summary>
+        /// <param name="targetNode">The target binary tree node.</param>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void PreorderTraverse(BinaryTreeNode<T> targetNode, Action<T> action = null)
+        {
+            if (targetNode == null)
+            {
+                return;
+            }
+
+            if (action != null)
+            {
+                action.Invoke(targetNode.element);
+            }
+
+            PreorderTraverse(targetNode.leftChild);
+            PreorderTraverse(targetNode.rightChild);
+        }
+
+        /// <summary>
+        /// Preorder traverse the binary tree.
+        /// </summary>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void PreorderTraverse(Action<T> action)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            if (m_root != null)
+            {
+                PreorderTraverse(m_root, action);
+            }
+        }
+
+        /// <summary>
+        /// Inorder traverse the binary tree.
+        /// </summary>
+        /// <param name="targetNode">The target binary tree node.</param>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void InorderTraverse(BinaryTreeNode<T> targetNode, Action<T> action = null)
+        {
+            if (targetNode == null)
+            {
+                return;
+            }
+
+            InorderTraverse(targetNode.leftChild);
+
+            if (action != null)
+            {
+                action.Invoke(targetNode.element);
+            }
+
+            InorderTraverse(targetNode.rightChild);
+        }
+
+        /// <summary>
+        /// Inorder traverse the binary tree.
+        /// </summary>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void InorderTraverse(Action<T> action)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            if (m_root != null)
+            {
+                InorderTraverse(m_root, action);
+            }
+        }
+
+        /// <summary>
+        /// Postorder traverse the binary tree.
+        /// </summary>
+        /// <param name="targetNode">The target binary tree node.</param>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void PostorderTraverse(BinaryTreeNode<T> targetNode, Action<T> action = null)
+        {
+            if (targetNode == null)
+            {
+                return;
+            }
+
+            PostorderTraverse(targetNode.leftChild);
+            PostorderTraverse(targetNode.rightChild);
+
+            if (action != null)
+            {
+                action.Invoke(targetNode.element);
+            }
+        }
+
+        /// <summary>
+        /// Postorder traverse the binary tree.
+        /// </summary>
+        /// <param name="action">The action to handle the element of the node.</param>
+        public void PostorderTraverse(Action<T> action)
+        {
+            if (action == null)
+            {
+                return;
+            }
+
+            if (m_root != null)
+            {
+                PostorderTraverse(m_root, action);
+            }
+        }
+
+        #endregion Traversing Functions
+
+        #endregion Public Functions
     }
 }
